5fa65fbae863e01e8c213351e2b7582a423aa170
Yurii-Vipshovsky <vip.cool.ua@gmail.com>
2023-10-25 17:59:00 +0300

    Add common admin user

--- Data/Migrations/20231025102537_AddRoles.cs
+++ Data/Migrations/20231025102537_AddRoles.cs
@@ -16,6 +16,20 @@ namespace GarageApp.Data.Migrations
             migrationBuilder.Sql($"INSERT INTO AspNetRoles (Id, Name, NormalizedName) VALUES ('{garageUserRoleId}', 'garageUser', 'GARAGEUSER')");
             migrationBuilder.Sql($"INSERT INTO AspNetRoles (Id, Name, NormalizedName) VALUES ('{garageOwnerRoleId}', 'garageOwner', 'GARAGEOWNER')");
             migrationBuilder.Sql($"INSERT INTO AspNetRoles (Id, Name, NormalizedName) VALUES ('{adminRoleId}', 'Admin', 'ADMIN')");
+
+            Guid userId = new Guid();
+            migrationBuilder.Sql($"INSERT INTO AspNetUsers (Id, UserName, " +
+                $"NormalizedUserName, Email, NormalizedEmail, PasswordHash, " +
+                $"SecurityStamp, ConcurrencyStamp, EmailConfirmed, PhoneNumberConfirmed, TwoFactorEnabled, " +
+                $"LockoutEnabled, AccessFailedCount) " +
+                $"VALUES ('{userId}', 'test@email.com', 'TEST@EMAIL.COM', " +
+                $"'test@email.com', 'TEST@EMAIL.COM'," +
+                $"'AQAAAAIAAYagAAAAEG42Ug7hTfqxDvhdhes/2rEfq36A+23jq87Nf/PLPu24YbZ9ifWl4DnzmNdua/4R5Q=='," +
+                $"'TFJ73JVFQ22CRHHJ5ABFWILBRH5KJY4Q', 'd69e1ac7-9ebd-435e-a206-7b2b68d42ce5', 'true', 'false', " +
+                $"'false', 'true', '0')");
+
+            migrationBuilder.Sql($"INSERT INTO AspNetUserRoles (UserId, RoleId) " +
+                $"VALUES ('{userId}', '{adminRoleId}')");
         }
 
         /// <inheritdoc />
