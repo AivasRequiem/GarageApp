0bc36a19f512a4c3004a37b3383df56c7203004d
Yurii-Vipshovsky <vip.cool.ua@gmail.com>
2023-10-29 18:31:48 +0200

    change errors, change views

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -26,7 +26,7 @@ namespace GarageApp.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel { RequestId = "Forse Error" });
         }
     }
 }
